f4f38cf72eb32c0fac08a6f5d5705f292170b22c
Gabriel Ferreira Santos <127545367+Gabriielll@users.noreply.github.com>
2023-05-10 12:35:29 -0300

    Add files via upload

---
+++ UrnaFuncional/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos:[
+        {
+            numero:'38111',
+            nome: 'Ryan Gosling',
+            partido: 'ABC',
+            fotos:[
+            {url:'Rg.jpg', legenda:'Vereador', small: false}
+            ]
+        },
+        {
+            numero:'77222',
+            nome: 'Michelle Yeoh',
+            partido: 'DEFG',
+            fotos:[
+            {url:'My.jpg', legenda:'Vereadora', small: false}
+            ]
+        },
+        ]
+},
+{
+    titulo: 'PREFEITO',
+    numeros:2,
+    candidatos:[
+        {
+            numero: '99',
+            nome: 'Leonardo DiCaprio',
+            partido: 'ABC',
+            vice: 'Brad Pitt',
+            fotos:[
+                {url: 'dc.jpg', legenda: 'Prefeito'},
+                {url: 'bradpit.jpg', legenda: 'Vice-prefeito', small: true}
+            ]
+        },
+        {
+            numero: '84',
+            nome: 'Ana Celia de Armas',
+            partido: 'QWRTY',
+            vice: 'Jake Gyllenhaal',
+            fotos:[
+                {url: 'Ana Armas.png', legenda: 'Prefeita'},
+                {url: 'jake.jpg', legenda: 'Vice-prefeito', small: true}
+            ]
+        },
+    ]
+}
+];
